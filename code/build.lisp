19242b384d46f48faab2e994dc46ae9d6194e748
Robert Burnett <dr.slate2@gmail.com>
2025-03-28 23:10:42 -0500

    some more experiments with build.lisp

--- code/build.lisp
+++ code/build.lisp
@@ -1,27 +1,39 @@
 #!/usr/bin/env sbcl --script
 
 (defun parse-argv ()
-  (let ((argv sb-ext:*posix-argv*))
-    (if (> (length argv) 1)
-      (intern (string-upcase (second argv)))
-      'test)
-    ))
+  (let ((arg (ignore-errors (second sb-ext:*posix-argv*))))
+    (if arg
+      (intern (string-upcase arg))
+      'all)))
+
 
 (defun build (arg)
-  (case arg
-    (all
-      (load #p"main.lisp")
-      (sb-ext:save-lisp-and-die "main"
-        :toplevel #'main; Set entry point
-        :executable t)    ; Create a standalone executable
+  (handler-case 
+    (ecase arg
+
+      ;;;; THE BUILD OPTIONS
+      (all
+        (build 'test))
+
+      (load 
+        (load #p"main.lisp"))
+
+      (compile
+        (build 'load)
+        (sb-ext:save-lisp-and-die
+          "main"
+          :toplevel #'main; Set entry point
+          :executable t)    ; Create a standalone executable
+        )
+
+      (test
+        (build 'load)
+        (run-tests)
+        )
       )
-    (test
-      (load #p"main.lisp")
-      (run-tests)
-      )
-    (t (format t "ERROR, expected 'all' or 'test'"))
-    )
+
+    ;;;; PRINT ERROR IF INVALID ARG PROVIDED
+    (t (e) (format t "~a" e)))
   )
 
 (build (parse-argv))
-(exit)
